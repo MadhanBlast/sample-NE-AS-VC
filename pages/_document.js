afc5622938a0a44a2a8f30379be01a582d4de70e
MadhanBlast <mk3696594@gmail.com>
2024-12-25 12:16:49 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Fullscreen meta tags for mobile */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#333" /> {/* Change the color to match your theme */}
+      </Head>
       <body>
         <Main />
         <NextScript />
